79a21c18b1efaadd4e04099fad33af7f4c0ca7ef
Refren86 <denis1975shvaiber@gmail.com>
2023-11-19 16:03:32 +0200

    Added aliases

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -2,8 +2,11 @@ import { Configuration } from "webpack";
 
 import { BuildOptions } from "./types/types";
 
-export function buildResolvers({}: BuildOptions): Configuration["resolve"] {
+export function buildResolvers({paths}: BuildOptions): Configuration["resolve"] {
   return {
     extensions: [".tsx", ".ts", ".js"], // to resolve imports without extensions
+    alias: {
+      "@": paths.src,
+    },
   };
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import { Suspense } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import { App } from "./components/App";
-import { About } from "./pages/about";
-import { Shop } from "./pages/shop";
+import { App } from "@/components/App";
+import { About } from "@/pages/about";
+import { Shop } from "@/pages/shop";
 
 const root = document.getElementById("root");
 
